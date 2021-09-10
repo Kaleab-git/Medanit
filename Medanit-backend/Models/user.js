f501cf57e127904c195785a2fae848ca98d000fc
SurafelTeka <50694227+SurafelTeka@users.noreply.github.com>
2021-09-10 19:26:28 +0300

    user registration validation done, user account edit validation done, user registration working,

--- Medanit-backend/Models/user.js
+++ Medanit-backend/Models/user.js
@@ -12,13 +12,13 @@ const User = mongoose.model('User', new mongoose.Schema({
         minLength: 0,
         maxLength: 50
     },
-    account_name: {
+    username: {
         type: String,
         required: true,
         unique: true,
         trim: true,
-        minLength: 0,
-        maxLength: 50
+        minLength: 1,
+        maxLength: 15
     },
     email: {
         type: String,
@@ -40,24 +40,43 @@ const User = mongoose.model('User', new mongoose.Schema({
         type: String,
         maxLength: 250
     },
-    date: {
+    joined: {
         type: Date,
         default: new Date(),
         get: date => moment(date, "YYYY-MM-DD[T00:00:00.000Z]").fromNow()
-    }
+    },
+
+    birthdate: {
+        type: Date,
+        get: date => moment(date).format('LL')
+    } 
 }));
+
+
+
 
 
 
 function validatePost(user) {
     const schema = Joi.object({
-    });
+        name: Joi.string().max(50).required(),
+        username: Joi.string().min(5).max(15).regex(/^@?(\w){1,15}$/).required(),
+        email: Joi.string().email().min(5).max(255).required(),
+        password: Joi.string().min(8).max(20).required(),
+        birthdate: Joi.date().max('01-01-2003').iso().messages({'date.format': `Date format is YYYY-MM-DD`,'date.max':`Age must be 18+`}).required()
+    })
 
     return schema.validate(user);
 };
 
 function validatePut(user) {
     const schema = Joi.object({
+        name: Joi.string().max(50).required(),
+        username: Joi.string().min(5).max(15).regex(/^@?(\w){1,15}$/),
+        email: Joi.string().email().min(5).max(255),
+        password: Joi.string().min(8).max(20),
+        bio: Joi.string().max(250),
+        profile_pic_url: Joi.string()
     });
     return schema.validate(user);
 }
--- Medanit-backend/routes/users.js
+++ Medanit-backend/routes/users.js
@@ -1,12 +1,59 @@
-const debug = require('debug')('app:users'); // set/export DEBUG=app:users
+const mongoose = require('mongoose');
+const _ = require('lodash');
+const bcrypt = require('bcrypt');
+const debug = require('debug')('app:users');        // set/export DEBUG=app:users
 const express = require('express');
-const Joi = require('joi');
-Joi.objectId = require('joi-objectid')(Joi)
+
+const { User, validatePost, validatePut, validateId } = require('../Models/user');
+
 const router = express.Router();
 
-/* GET users listing. */
-router.get('/', (req, res) => {
-    res.send('Users get method');
+
+/* Register a user */
+router.post('/', async (req, res) => {
+    const { error, value } = validatePost(req.body);
+    let result;
+    if (error) return res.status(400).send(error.message);
+
+    
+
+    try{
+
+        let user = await User.findOne( { email: value.email } )
+        if(user) return res.status(400).send('Email already taken.');
+        
+        user = await User.findOne( { username: value.username } )
+        if(user) return res.status(400).send('Username already taken.')
+
+
+        user = new User({
+            ...value,
+            joined: new Date()
+        });
+
+
+        user.validate(async (err) => {
+            if(err){
+                return res.status(400).send(err.message);
+            }else{  
+                try{
+                    const salt = await bcrypt.genSalt(10);
+                    user.password = await bcrypt.hash(user.password, salt);
+                    result = await user.save();
+                    return res.status(201).send(_.pick(result, ['_id', 'name', 'username', 'email']));
+                }catch(err){
+                    debug(err);
+                    return res.status(500).send("Internal server error while trying to create a new post!");
+                }
+            }
+        });
+        
+        
+    }catch(err){
+        debug(err.message);
+        return res.status(500).send("Internal server error while trying to create a new post!");
+    }
+
 });
 
 
