de119706987a49d8a4b3b38a2649ba04fb1c8d1d
Yohannes a <81992284+yohannesaddisu@users.noreply.github.com>
2021-09-10 16:02:26 +0300

    Added Navigator with Post data passed as an argument

--- medanit_frontend/lib/screens/feed.dart
+++ medanit_frontend/lib/screens/feed.dart
@@ -39,4 +39,114 @@ class _FeedState extends State<Feed> {
         context.read<PostBloc>().add(PostUpdate(post, action));
       });
     }
-  
+  return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromRGBO(209, 117, 129, 1),
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pushNamed(context, Doctor.routeName);
+          },
+          icon: Icon(Icons.person),
+        ),
+        actions: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(right: 20.0),
+            child: Icon(Icons.more_vert),
+          ),
+        ],
+      ),
+  body: BlocBuilder<PostBloc, PostState>(
+        builder: (_, state) {
+          if (state is PostFail) {
+            return Text('Error while performing operation');
+          }
+
+          if (state is PostsLoadSuccess) {
+            final posts = state.posts;
+
+            return ListView.builder(
+              itemCount: posts.length,
+              itemBuilder: (_, idx) => ListTile(
+                subtitle: Container(
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Container(
+                            child: Row(
+                              children: [
+                                Icon(Icons.person, size: 50),
+                                Padding(
+                                  padding: EdgeInsets.only(left: 10),
+                                  child: (Column(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        'Segni A',
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.black,
+                                          fontSize: 13,
+                                        ),
+                                      ),
+                                      RichText(
+                                          text: TextSpan(children: <TextSpan>[
+                                        TextSpan(
+                                          text: '@segniAdeba',
+                                          style: TextStyle(color: Colors.blue),
+                                        ),
+                                        TextSpan(
+                                            text: '  $bullet ',
+                                            style:
+                                                TextStyle(color: Colors.black)),
+                                        TextSpan(
+                                            text: '${posts[idx].date}',
+                                            style: TextStyle(
+                                                color: Colors.grey,
+                                                fontSize: 13))
+                                      ])),
+                                    ],
+                                  )),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Padding(
+                              padding: EdgeInsets.only(right: 15),
+                              child: Icon(Icons.bookmark_border))
+                        ],
+                      ),
+                      Padding(
+                          // '${posts[idx].title} \n ${posts[idx].description}'
+                          padding: EdgeInsets.only(
+                              left: 50, right: 50, bottom: 10, top: 10),
+                          child: RichText(
+                            text: TextSpan(
+                              children: <TextSpan>[
+                                // *** Added Navigator with Post data passed as an argument
+                                TextSpan(
+                                    text: '${posts[idx].title} \n',
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.black),
+                                    recognizer: TapGestureRecognizer()
+                                      ..onTap = () {
+                                        Navigator.of(context).pushNamed(
+                                            PostDetail.routeName,
+                                            arguments: posts[idx].id);
+                                      }),
+                                TextSpan(
+                                    text: '${posts[idx].description}',
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                    ),
+                                    recognizer: TapGestureRecognizer()
+                                      ..onTap = () {
+                                        Navigator.of(context).pushNamed(
+                                            PostDetail.routeName,
+                                            arguments: posts[idx].id);
+                                      }),
+                              ],
+                            ),
+                          )),
