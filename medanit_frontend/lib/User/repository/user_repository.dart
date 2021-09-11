81f4076e05d1ec1b3bbe7b4b742b7007a0405d82
Segni Adeba <61460186+SegniAdebaGodsSon@users.noreply.github.com>
2021-09-11 20:20:48 +0300

    User Repository done

---
+++ medanit_frontend/lib/User/repository/user_repository.dart
@@ -0,0 +1,29 @@
+import 'package:medanit_frontend/Posts/models/models.dart';
+import 'package:meta/meta.dart';
+import 'package:medanit_frontend/User/user.dart';
+
+class UserRepository {
+  final UserDataProvider dataProvider;
+
+  UserRepository({required this.dataProvider});
+
+  Future<List<Post>> userPosts(String user_id) async {
+    return await dataProvider.userPosts(user_id);
+  }
+
+  Future<User> usersInfo() async {
+    return await dataProvider.usersInfo();
+  }
+
+  // Future<List<Post>> getPost(String postid) async {
+  //   return await dataProvider.getPost(postid);
+  // }
+
+  Future<void> updateUser(User user) async {
+    await dataProvider.updateUser(user);
+  }
+
+  Future<void> deletePost() async {
+    await dataProvider.deletePost();
+  }
+}
